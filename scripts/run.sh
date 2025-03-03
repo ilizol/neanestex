70db193bd58334d3c9c4e78b9992813cbde1c3b8
Elias Zolotas <ilizol@yahoo.gr>
2025-03-03 13:45:15 +0200

    [WIP] Support Almouzios font.

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+# neanes
+# ~/Projects/neanes/dist/neanes-cli --silent-latex ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
+
+# neanes-examples
+# cp -f ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx ~/Projects/neanestex/examples/kontakion_prophet_elias_almouzios.byzx
+# cp -f ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byztex ~/Projects/neanestex/examples/kontakion_prophet_elias_almouzios.byztex
+
+# neanestex (lualatex)
+lualatex kontakion_prophet_elias_almouzios.tex
